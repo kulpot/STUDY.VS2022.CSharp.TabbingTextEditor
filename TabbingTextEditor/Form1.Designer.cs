5eeb01d9b889a98057cb65f034209efa6d4d923f
Kulpot <kulpotwahu@gmail.com>
2023-05-23 03:53:21 +0800

    Add project files.

---
+++ TabbingTextEditor/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace TabbingTextEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textualTabControl1 = new TabbingTextEditor.TextualTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textualTabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textualTabControl1
+            // 
+            this.textualTabControl1.Controls.Add(this.tabPage1);
+            this.textualTabControl1.Controls.Add(this.tabPage2);
+            this.textualTabControl1.Location = new System.Drawing.Point(12, 40);
+            this.textualTabControl1.Name = "textualTabControl1";
+            this.textualTabControl1.SelectedIndex = 0;
+            this.textualTabControl1.Size = new System.Drawing.Size(331, 207);
+            this.textualTabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(323, 181);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Get Text";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 247);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textualTabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.textualTabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TextualTabControl textualTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TabbingTextEditor/TextualTabControl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TabbingTextEditor
+{
+    class TextualTabControl : TabControl
+    {
+        protected override void OnControlAdded(ControlEventArgs e)
+        {
+            base.OnControlAdded(e);
+            e.Control.Controls.Add(CreateTextBox());
+        }
+
+        private TextBox CreateTextBox()
+        {
+            TextBox textBox = new TextBox();
+            textBox.Multiline = true;
+            textBox.Dock = DockStyle.Fill;
+            // Add additional configurations here
+            return textBox;
+        }
+
+        public string SelectedTextBoxText
+        {
+            get { return this.SelectedTab.Controls[0].Text; }
+        }
+
+        public bool IsSelectedTextBoxMultiline
+        {
+            get { return ((TextBox)this.SelectedTab.Controls[0]).Multiline; }
+        }
+    }
+}
